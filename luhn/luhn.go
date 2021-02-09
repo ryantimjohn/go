1a4860208f8d20d9348908320867c78bb32b8a3c
ryantimjohn <tim@alloy.us>
2021-02-09 11:03:05 -0500

    use boolean

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -9,22 +9,24 @@ import (
 func Valid(s string) bool {
 	stripped := strings.ReplaceAll(s, " ", "")
 	length := len(stripped)
+	double := length%2 == 0
 	if length < 2 {
 		return false
 	}
 	sum := 0
-	for i, c := range stripped {
+	for _, c := range stripped {
 		if !(unicode.IsNumber(c)) {
 			return false
 		}
 		n := int(c) - 48
-		if (length-i)%2 == 0 {
+		if double {
 			n *= 2
 		}
 		if n > 9 {
 			n -= 9
 		}
 		sum += n
+		double = !double
 	}
 	if sum%10 != 0 {
 		return false
